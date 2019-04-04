8f1a838ac0a9cc4beae1bf65bfbc9f4b1da5ab3f
tkanas <kanas.tomasz@gmail.com>
2019-04-04 11:35:19 +0200

    run32.s

---
+++ zad1/run32.s
@@ -0,0 +1,38 @@
+.global run32
+.type run32, @function
+
+.code32
+switch32:
+    pushl $0x2b
+    popl %ds
+    pushl $0x2b
+    popl %es
+
+    //setup arguments
+    //program name
+    leal 8(%esp), %eax
+    pushl %eax
+    //argc
+    pushl $1
+    pushl %edi
+    ret
+    
+    //It won't return - this code is for recycling
+    pushl $0x33
+    pushl $back64
+    lret
+    
+.code64
+run32:
+    mov %rsp, %r11
+    movq %rsi, %rsp
+    subq $8, %rsp
+    movq %r11, 8(%rsp)
+    movl $0x23, 4(%rsp)
+    movq $switch32, %rax
+    movl %eax, (%rsp)
+    lret
+
+back64:
+    pop %rsp
+    ret
