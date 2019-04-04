b146e8490cbfd3fc95f10f99381722bbdc7b0d8f
tkanas <kanas.tomasz@gmail.com>
2019-04-04 12:03:26 +0200

    lab3 reformat

--- lab3/formatter.c
+++ lab3/formatter.c
@@ -7,7 +7,6 @@
 
 typedef void (*formatter) (int);
 typedef void (*printf_t) (char*, int);
-//typedef int (*int_f) (int);
 
 void print_int(char *format, int param) {
     printf(format, param);
@@ -26,12 +25,14 @@ formatter make_formatter(const char *format) {
         printf("Błąd podczas czytania pliku");
         exit(1);
     }
-    void* f = mmap(NULL, 40, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE, fd, 0);
+    void* f = mmap(NULL, 26, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE, fd, 0);
     close(fd);
     //int* ftab = (int*)f;
     //printf("%x %x %x %x", ftab[0], ftab[1], ftab[2], ftab[3]);
-    ((char**)f)[1] = form;
-    ((printf_t*)f)[3] = *print_int;
+    //((char**)f)[1] = form;
+    //((printf_t*)f)[3] = *print_int;
+    memcpy((char*)f + 4, (void*)&form, 8);
+    memcpy((char*)f + 14, (void*)&*print_int, 8);
     return (formatter)f;
 }
 
@@ -46,9 +47,5 @@ int main() {
     d_format (0x9abc);
     verbose_format (0xdef0);
 
-    //int square_fd = open("square.o", O_RDONLY);
-    //int_f square = (int_f) mmap(NULL, 17, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE, square_fd, 0);
-    //int x = square(5);
-    //printf("%d\n", x);
     return 0;
 }
--- lab3/tramp.asm
+++ lab3/tramp.asm
@@ -1,14 +1,6 @@
 .text:
-    sub rsp, 8
     mov esi, edi
     mov rdi, strict qword 0
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
     mov rax, strict qword 0
-    call rax
-    add rsp, 8
-    ret
+    jmp rax
+
